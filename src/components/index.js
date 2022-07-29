3498e2d0eb9b3cbfb5fcaf8ca248be7998cf96d3
Nic Jackson <jackson.nic@gmail.com>
2022-07-29 08:03:53 +0100

    Re-org structure

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import VSCodeBrowser from './VSCodeBrowser';
+import VSCodeFile from './VSCodeFile';
+import VSCodeTerminal from './VSCodeTerminal';
+
+export { VSCodeBrowser, VSCodeFile, VSCodeTerminal };
